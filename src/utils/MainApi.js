5d4b7aafcf113e3fe8aa91c2724defa96f9c9599
Andrey Zhukov <settler59@yandex.ru>
2023-01-03 16:29:32 +0100

    fix: поправил base url на удаленный

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -74,7 +74,7 @@ class Api {
 
 const MainApi = 
 new Api(  
-  {baseUrl: 'http://localhost:4000'
+  {baseUrl: 'https://api.diploma.zhukov.nomoredomains.club'
   }
 )
 
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 // export const BASE_URL = 'https://api.diploma.zhukov.nomoredomains.club';
 
 
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://api.diploma.zhukov.nomoredomains.club';
 
 function request ({
   url,
